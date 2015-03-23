0cd77749632d2d9e67652ce74790483e24321dea
neo.alienson <neo@orz.hk>
2015-03-23 16:20:49 +0800

    fix import

--- src/test/java/com/instastore/service/CheckoutServiceV2Test.java
+++ src/test/java/com/instastore/service/CheckoutServiceV2Test.java
@@ -1,5 +1,6 @@
 package com.instastore.service;
 import org.junit.Test;
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
